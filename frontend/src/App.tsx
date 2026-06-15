5ad21452956417189c4901f7c41c1bb42504509a
huutamit789-dev <huutamit789@gmail.com>
2026-06-15 23:17:04 +0700

    config frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,7 @@
-import { useEffect } from 'react';
-import axiosClient from './api/axiosClient';
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
-  useEffect(() => {
-    axiosClient.get('/products') // Thay bằng endpoint của bạn
-      .then(res => console.log('Kết nối thành công:', res.data))
-      .catch(err => console.log('Lỗi kết nối:', err));
-  }, []);
-
-  return <h1>Hello ElectricStore</h1>;
+  return <AppRoutes />;
 }
 
 export default App;
