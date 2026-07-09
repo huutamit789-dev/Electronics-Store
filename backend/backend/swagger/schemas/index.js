5027d040eb1ee098443221709ea078b1f5ac9235
huutamit789-dev <huutamit789@gmail.com>
2026-07-09 09:57:59 +0700

    Fix CORS for all Vercel domains, update backend URL

---
+++ backend/backend/swagger/schemas/index.js
@@ -0,0 +1,20 @@
+const User = require('./UserSchema');
+const Product = require('./ProductSchema');
+const Order = require('./OrderSchema');
+const Cart = require('./CartSchema');
+const Payment = require('./PaymentSchema');
+const Review = require('./ReviewSchema');
+const Common = require('./CommonSchema');
+const Categories = require('./CategoriesSchema');
+const Footer = require('./FooterSchema');
+module.exports = {
+  ...User,
+  ...Product,
+  ...Order,
+  ...Cart,
+  ...Payment,
+  ...Review,
+  ...Common,
+  ...Categories,
+  ...Footer
+};
